5980ca297e057bef8c9b81a48248ce50cf762207
Mikail Gedik <mikail.gedik@bluewin.ch>
2019-10-01 15:43:44 +0200

    asdf

--- src/ch/ww/electronics/game/gameobject/Brain.java
+++ src/ch/ww/electronics/game/gameobject/Brain.java
@@ -1,9 +1,16 @@
 package ch.ww.electronics.game.gameobject;
 
+import java.util.Random;
+
+import ch.ww.electronics.util.Vector2D;
+
 public class Brain {
 	public enum Status {IDLE, CHASING, EATING, SEARCHING_FOOD};
 	
 	private final Animal animal;
+	private final Random r;
+	private final DNA dna;
+	
 	private final Sensors sensors;
 	
 	private Status status;
@@ -13,6 +20,8 @@ public class Brain {
 	public Brain(Animal animal) {
 		this.animal = animal;
 		this.sensors = new Sensors(this.animal);
+		r = animal.getRandom();
+		dna = animal.getDNA();
 		this.target = null;
 		status = Status.IDLE;
 	}
@@ -20,10 +29,17 @@ public class Brain {
 	public void think() {
 		switch(status) {
 		case IDLE:
+			if(animal.getMotion().getLength() == 0 && animal.getLevel().getRandom().nextDouble() < 0.1) {
+				animal.setMotion(new Vector2D());
 			
+			} else if(animal.getLevel().getRandom().nextDouble() < 0.1) {
+				
+			}
+			break;
 		case CHASING:
-			
+			break;
 		case EATING:
+			break;
 		case SEARCHING_FOOD:
 			break;
 		default:
