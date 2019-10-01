72aac9cb3a4c5d74f08b347a6d439e100912ba39
Neuronal1 <55983600+Neuronal1@users.noreply.github.com>
2019-10-01 15:00:29 +0200

    Sensors

--- src/ch/ww/electronics/game/gameobject/Sensor.java
+++
@@ -1,9 +0,0 @@
-package ch.ww.electronics.game.gameobject;
-
-public abstract class Sensor{
-	protected Animal animal;
-	public Sensor(Animal animal){
-		this.animal=animal;
-	}
-	public abstract void getdata();
-}
---
+++ src/ch/ww/electronics/game/gameobject/Sensors.java
@@ -0,0 +1,16 @@
+package ch.ww.electronics.game.gameobject;
+
+import java.util.ArrayList;
+
+public class Sensors{
+	private Animal animal;
+	public Sensors(Animal animal){
+		this.animal=animal;
+	}
+	public ArrayList<Animal> getEyeInput(){
+		
+		return null;
+	}
+	
+	
+}
