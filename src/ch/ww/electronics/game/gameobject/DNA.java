8646e8bfe8d258ed40da198d88ac8954e4fa47de
Neuronal1 <55983600+Neuronal1@users.noreply.github.com>
2019-10-01 14:15:53 +0200

    DNA prof fertig

--- src/ch/ww/electronics/game/gameobject/DNA.java
+++ src/ch/ww/electronics/game/gameobject/DNA.java
@@ -20,21 +20,32 @@ public class DNA {
 		this.speed=speed;
 		this.viewrange=viewrange;
 	}
+	public DNA(DNA dna){
+		this.game=dna.game;
+		
+		this.size=dna.getSize();
+		this.fur=dna.getFur();
+		this.speed=dna.getSpeed();
+		this.viewrange=dna.getViewrange();
+	}
 	
 	public void randomize(){
-		size=(Math.random()*2-1);
-		fur=(Math.random()*2-1);
-		speed=(Math.random()*2-1);
-		viewrange=(Math.random()*2-1);
+		size=(game.getRandom().nextDouble()*2-1);
+		fur=(game.getRandom().nextDouble()*2-1);
+		speed=(game.getRandom().nextDouble()*2-1);
+		viewrange=(game.getRandom().nextDouble()*2-1);
 	}
 	
 	public void variate(double strahlung){
-		size+=(Math.random()*2-1)*variation*(1+strahlung);
-		fur+=(Math.random()*2-1)*variation*(1+strahlung);
-		speed+=(Math.random()*2-1)*variation*(1+strahlung);
-		viewrange+=(Math.random()*2-1)*variation*(1+strahlung);
+		size+=(game.getRandom().nextDouble()*2-1)*variation*(1+strahlung);
+		fur+=(game.getRandom().nextDouble()*2-1)*variation*(1+strahlung);
+		speed+=(game.getRandom().nextDouble()*2-1)*variation*(1+strahlung);
+		viewrange+=(game.getRandom().nextDouble()*2-1)*variation*(1+strahlung);
 	}
 
+	
+	
+	
 	public double getSize() {
 		return size;
 	}
