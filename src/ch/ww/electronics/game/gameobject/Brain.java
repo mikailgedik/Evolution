59a34ffdd0f8fef1d8dce283d143628b0ce5c7a0
wolkenwalker <mikail.gedik@bluewin.ch>
2019-10-02 12:59:16 +0200

    sortiert

--- src/ch/ww/electronics/game/gameobject/Brain.java
+++ src/ch/ww/electronics/game/gameobject/Brain.java
@@ -36,52 +36,61 @@ public class Brain {
 		ArrayList<Animal> nearby = sensors.getEyeInput();
 		if(animal.getEnergy()/dna.getMaxEnergy() < 1 && status != Status.CHASING) {
 			status = Status.SEARCHING_FOOD;
-			System.out.println("Hungry");
 		}
 		status=Status.IDLE;
 		switch(status) {
 		case IDLE:
-			
-			if(animal.getMotion().getLength() == 0 && animal.getLevel().getRandom().nextDouble() < 0.1) {
-				double maxspeed = dna.getSize();
-				double speed=animal.getGame().getRandom().nextDouble()*maxspeed;
-				double  angle=animal.getGame().getRandom().nextDouble()*2*Math.PI;
-				animal.setMotion(new Vector2D(speed*Math.sin(angle),speed*Math.cos(angle)));
-			} else if(animal.getLevel().getRandom().nextDouble() < 0.1) {
-				animal.setMotion(new Vector2D(0,0));
-			}
-			if(getAnimal().getEnergy()/getAnimal().getDNA().getMaxEnergy()>0.8){
-				Animal baby = new Animal(animal.getLevel(), animal.getX(), animal.getY());
-				baby.getDNA().variate(0);
-				baby.setEnergy(baby.getEnergy()/2);
-				getAnimal().setEnergy(getAnimal().getEnergy()/3);
-				
-			}
+			idle(nearby);
 			break;
 		case CHASING:
-			if(this.animal.isTouching(target)) {
-				animal.getLevel().fight(this.animal, target);
-				System.out.println("Kill");
-				status = Status.IDLE;
-			} else {
-				MutableVector2D v = new MutableVector2D(target.getX() - this.animal.getX(), target.getY() - this.animal.getY());
-				double factor = dna.getMaxSpeed() / v.getLength();
-				v = new MutableVector2D(v.getX() * factor, v.getY() * factor);
-				animal.setMotion(v);
-				break;
-			}
+			chasing(nearby);
+			break;
 		case SEARCHING_FOOD:
-			if(nearby.size() > 0) {
-				target = nearby.get(animal.getRandom().nextInt(nearby.size()));
-				status = Status.CHASING;
-			} else {
-				status = Status.IDLE;
-				target = null;
-			}
-			
+			searchingFood(nearby);
 			break;
 		default:
 			throw new RuntimeException("Should not reach this step");
+		}
+	}
+	
+	private void idle(ArrayList<Animal> nearby) {
+		if(animal.getMotion().getLength() == 0 && animal.getLevel().getRandom().nextDouble() < 0.1) {
+			double maxspeed = dna.getSize();
+			double speed=animal.getGame().getRandom().nextDouble()*maxspeed;
+			double  angle=animal.getGame().getRandom().nextDouble()*2*Math.PI;
+			animal.setMotion(new Vector2D(speed*Math.sin(angle),speed*Math.cos(angle)));
+		} else if(animal.getLevel().getRandom().nextDouble() < 0.1) {
+			animal.setMotion(new Vector2D(0,0));
+		}
+		if(getAnimal().getEnergy()/getAnimal().getDNA().getMaxEnergy()>0.8){
+			Animal baby = new Animal(animal.getLevel(), animal.getX(), animal.getY());
+			baby.getDNA().variate(0);
+			baby.setEnergy(baby.getEnergy()/2);
+			getAnimal().setEnergy(getAnimal().getEnergy()/3);
+			
+		}
+	}
+	
+	private void chasing(ArrayList<Animal> nearby) {
+		if(this.animal.isTouching(target)) {
+			animal.getLevel().fight(this.animal, target);
+			System.out.println("Kill");
+			status = Status.IDLE;
+		} else {
+			MutableVector2D v = new MutableVector2D(target.getX() - this.animal.getX(), target.getY() - this.animal.getY());
+			double factor = dna.getMaxSpeed() / v.getLength();
+			v = new MutableVector2D(v.getX() * factor, v.getY() * factor);
+			animal.setMotion(v);
+		}
+	}
+	
+	private void searchingFood(ArrayList<Animal> nearby) {
+		if(nearby.size() > 0) {
+			target = nearby.get(animal.getRandom().nextInt(nearby.size()));
+			status = Status.CHASING;
+		} else {
+			status = Status.IDLE;
+			target = null;
 		}
 	}
 	
