f0719f211e42c44f8ac1a854c5d78bd187328aa2
Neuronal1 <55983600+Neuronal1@users.noreply.github.com>
2019-10-03 15:06:10 +0200

    Hey milchkamel

--- src/ch/ww/electronics/game/gameobject/DNA.java
+++ src/ch/ww/electronics/game/gameobject/DNA.java
@@ -43,7 +43,7 @@ public class DNA {
 		values.put(MAX_ENERGY,20000d);
 		values.put(STUNNED_TIME,0.5);
 		values.put(RUNNING_TIME, 0.5);
-		values.put(START_SEARCHING_FOOD,1d);
+		values.put(START_SEARCHING_FOOD,10d);
 		values.put(BABY_WHEN_ENERGIE, 0.5);
 	}
 	
