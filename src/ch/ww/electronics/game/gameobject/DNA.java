f39199c42f340e4f452a9c158e246e329ad77c2c
Neuronal1 <55983600+Neuronal1@users.noreply.github.com>
2019-10-03 08:58:16 +0200

    dna verbessert

--- src/ch/ww/electronics/game/gameobject/DNA.java
+++ src/ch/ww/electronics/game/gameobject/DNA.java
@@ -21,6 +21,7 @@ public class DNA {
 		this.viewrange = viewrange;
 		this.maxEnergy = maxEnergy;
 		this.stunned = stunned;
+		make_valid();
 	}
 	public DNA(Animal animal){
 		this.animal=animal;
@@ -34,6 +35,7 @@ public class DNA {
 		viewrange = prandom() * 5;
 		maxEnergy = prandom() * 100;
 		stunned = animal.getRandom().nextDouble()*200;
+		make_valid();
 	}
 	private double prandom(){
 		return(animal.getRandom().nextDouble()*0.5+0.5);
@@ -44,6 +46,15 @@ public class DNA {
 		fur += (animal.getRandom().nextDouble() * 2 - 1) * VARIATION * (1 + radiation);
 		maxSpeed += (animal.getRandom().nextDouble() * 2 - 1) * VARIATION * (1 + radiation);
 		viewrange += (animal.getRandom().nextDouble() * 2 - 1) * VARIATION * (1 + radiation);
+		stunned += (animal.getRandom().nextDouble() * 2 - 1) * VARIATION * (1 + radiation)*10;
+		make_valid();
+	}
+	private void make_valid(){
+		if(size<0.5)size=1;
+		if(fur<0)fur=0; if(fur>1)fur=1;
+		if(maxSpeed<0)maxSpeed=0;
+		if(viewrange<0)viewrange=0;
+		if(stunned<0)stunned=0;
 	}
 
 	public double getSize() {
