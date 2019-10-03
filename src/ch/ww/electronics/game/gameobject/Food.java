cbb8ad999c4621dc32b6eeccd5a669eda0d44084
wolkenwalker <mikail.gedik@bluewin.ch>
2019-10-03 08:39:45 +0200

    food

--- src/ch/ww/electronics/game/gameobject/Food.java
+++ src/ch/ww/electronics/game/gameobject/Food.java
@@ -1,12 +1,17 @@
 package ch.ww.electronics.game.gameobject;
 import ch.ww.electronics.game.level.Level;
+import ch.ww.electronics.graphics.Screen;
+import ch.ww.electronics.level.backgroundtile.BackgroundTile;
 
 public class Food extends Animal{
 	public Food(Level level, double x, double y) {
 		super(level, x, y);
 		setStatus(Brain.Status.BE_FOOD);
 		
-		
+		Screen s = new Screen((int) (BackgroundTile.SIZE * getDNA().getSize()), (int) (BackgroundTile.SIZE * getDNA().getSize()),
+				-1);
+		s.fillCircle(0, 0, 0x00ff00, s.getHeight()/2);
+		setTexture(s.darkScreen(getEnergy()/getDNA().getMaxEnergy()));
 	}
 	@Override public void tick(){}
 }
