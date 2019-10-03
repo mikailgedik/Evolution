59f2c8d168d406991b272b92cb6109bea3f6e81c
Neuronal1 <55983600+Neuronal1@users.noreply.github.com>
2019-10-03 09:01:06 +0200

    dna

--- src/ch/ww/electronics/game/gameobject/DNA.java
+++ src/ch/ww/electronics/game/gameobject/DNA.java
@@ -22,7 +22,7 @@ public class DNA {
 		this.maxEnergy = maxEnergy;
 		this.stunned = stunned;
 		make_valid();
-		System.out.print();
+		System.out.println();
 	}
 	public DNA(Animal animal){
 		this.animal=animal;
